c0e948b4126a74466e1a86eb33373472b2fd5278
Setu Potnis <setupotnis@gmail.com>
2020-04-12 17:36:17 -0400

    added navigation from the welcome screen to the log in and registration screens

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/welcome_screen.dart';
@@ -23,9 +26,9 @@ class FlashChat extends StatelessWidget {
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'registration_screen': (context) => RegistrationScreen(),
-        'chat_screen': (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
